14c768708106b157f2df311546a5adca4e2c3650
Miguel Portes <miguelszportes@gmail.com>
2024-10-22 16:47:54 -0300

    Add files via upload

--- 1. Recursividade/Ex_3.java
+++ 1. Recursividade/Ex_3.java
@@ -12,16 +12,24 @@ public class Ex_3 {
             System.exit(1);
         }
 
-        boolean ePrimo = calculo(n, n-1);
+        if(n == 1 || n==0)
+            System.out.println("Não é primo!");
+        else{
+            boolean ePrimo = calculo(n, n-1);
 
-        System.out.println((ePrimo)? n + " é primo!" : n + " não é primo!"); 
-
+            System.out.println((ePrimo)? n + " é primo!" : n + " não é primo!"); 
+        }
         teclado.close();
     }
 
-    public static boolean calculo(int n, int i){
-       // se algum número sem ser 1, ou o próprio n, até aquele valor tiver resto 0, ele n é primo
-
+    public static boolean calculo(int num, int divisor){
+       // se algum número sem ser 1, ou o próprio n até aquele valor, tiver resto 0, ele n é primo
+       if(divisor == 1)
+            return true; 
+       if(num%divisor == 0){
+            return false;
+        }            
+        return calculo(num, divisor-1);
        
     } 
 }
---
+++ 1. Recursividade/Ex_4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ex_4 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Informe dois números: ");
+        int n1=0, n2=0;
+        try {
+            n1 = teclado.nextInt();
+            n2 = teclado.nextInt();
+        } catch (Exception e) {
+            System.out.println("Insira um número inteiro! ");
+        }
+
+        int resultado = calculo(n1, n2);
+
+        System.out.printf("\n%d*%d = %d",n1,n2, resultado);
+        teclado.close();
+    }
+    public static int calculo(int n1, int n2){
+        if(n2 == 1)
+            return n1;
+        return n1 + calculo(n1, n2-1);
+    }
+     
+}
