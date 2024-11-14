9592c10b613ff62e22fe1bfc841af7c5892ba68c
Miguel <miguelszportes@gmail.com>
2024-11-13 23:06:33 -0300

    Atividade 4

--- 3.Op_com_vet/Atividade_4.java
+++ 3.Op_com_vet/Atividade_4.java
@@ -1,24 +1,87 @@
 import java.util.Scanner;
 
 public class Atividade_4 {
+    static int[] vet = new int[10];
+    static int ulPosicao = -1; 
+    static boolean condicao = false;   
+
     public static void main(String[] args) {
         Scanner tec = new Scanner(System.in);
-        int[] vet = new int[10];
-        int valor = 0, opc = 0, uPosicao = -1;        
-        System.out.println("Preencha um vetor!");
+        System.out.println("Manipule um vetor!");
 
-        for (int i = 0; i < vet.length; i++) {
-            System.out.print("Insira um valor: ");
-            valor = tec.nextInt();
-            System.out.println("Em qual posição você quer inserir? ");
+        int posicao = -1;
+        
+        try {
+            
+        while (!condicao) {
+            System.out.print("\n1-Insira algum valor\n2-Apague algum valor\n3-Sair\nEscolha: ");
+            int opcao = tec.nextInt();
 
-            if(i == 0){
-                System.out.println("1-Início\n2-Fim");
-                opc = tec.nextInt();
+            if (opcao == 1) {
+                System.out.print("Valor: ");
+                int valor = tec.nextInt();
+
+                System.out.println("Em qual posição você quer inserir? Entre 0 e " + (ulPosicao + 1));
+                posicao = tec.nextInt();
+
+                while (posicao < 0 || posicao > ulPosicao + 1) {
+                    System.out.println("Em qual posição você quer inserir? Entre 0 e " + (ulPosicao + 1));
+                    posicao = tec.nextInt();
+                }
+
+                inserir_no_vetor(posicao, valor);
+            }  
+            else if (opcao == 2) {
+                if(ulPosicao != -1 ){
+                    System.out.println("Qual posição você quer excluir? Entre 0 e " + (ulPosicao++));
+                    posicao = tec.nextInt();
+                    while (posicao < 0 || posicao > ulPosicao) {
+                        System.out.println("Qual posição você quer excluir? Entre 0 e " + ulPosicao);
+                        posicao = tec.nextInt();
+                    }
+                    deletar_no_vetor(posicao);
+                }else{
+                    System.out.println("Não há posições para serem excluídas!");
+                }
+            }
+            else if (opcao == 3) {
+                condicao = true;
+            }
+            else {
+                System.out.println("Opção inválida!");
             }
 
+            System.out.println("Vetor atualizado:");
+            for (int i = 0; i <= ulPosicao; i++) {
+                System.out.print(vet[i] + "|");
+            }
+            System.out.println();
         }
+    } catch (Exception e) {
+        System.out.println("Reinicie o programa, e informe valores numéricos inteiros dessa vez!");
+    }
+        tec.close();
     }
 
+    public static void inserir_no_vetor(int posicao, int valor) {
+        if (ulPosicao >= vet.length - 1){
+            condicao = true;
+            return;
+        }
+        for (int i = ++ulPosicao; i > posicao; i--) {
+            vet[i] = vet[i - 1];
+        }
+        vet[posicao] = valor;
+    }
 
+    public static void deletar_no_vetor(int posicao) {
+        if (ulPosicao == -1) {
+            System.out.println("Vetor vazio!");
+            return;
+        }
+        for (int i = posicao; i < ulPosicao; i++) {
+            vet[i] = vet[i + 1];
+        }
+        ulPosicao--;
+    }
 }
--- 3.Op_com_vet/Atividade_5.java
+++ 3.Op_com_vet/Atividade_5.java
@@ -1,3 +1,20 @@
 public class Atividade_5 {
-    
+    public static void main(String[] args) {
+        int[] vet = {0, 999, 1099, 1, 2, 4, -1, 990, 18};
+        System.out.println("Ordenando o vetor: ");
+        ordenar_sem_ordenar(vet);
+
+    }
+    public static void ordenar_sem_ordenar(int[] vet){
+        int min = 0, max = 0;
+        for (int i = 0; i < vet.length; i++) {
+            for (int j = 0; j < vet.length; j++) {
+                if(vet[i] < vet[j])
+                    min = vet[i];
+                if(vet[i] > vet[j])
+                    max = vet[i];
+            }
+        System.out.println(""+ min + "|"+max);
+        }
+    }
 }
