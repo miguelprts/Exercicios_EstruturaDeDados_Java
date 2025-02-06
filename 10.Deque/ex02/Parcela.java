6029f25978b4c74cfa20c60395e98a645993f491
Miguel Portes <miguelszportes@gmail.com>
2025-02-06 09:26:45 -0300

    Create Parcela.java

---
+++ 10.Deque/ex02/Parcela.java
@@ -0,0 +1,17 @@
+package ex02;
+
+public class Parcela {
+    private double valor;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Parcela(double valor) {
+        this.valor = valor;
+    }
+}
